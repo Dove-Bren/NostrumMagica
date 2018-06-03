3071d4bc61972390b3782221d36f8f86d1e9d021
Skyler Manzanares <skymanzanares@hotmail.com>
2018-06-03 16:26:20 -0700

    Fixed touch. Whoops

--- src/main/java/com/smanzana/nostrummagica/spells/components/triggers/TouchTrigger.java
+++ src/main/java/com/smanzana/nostrummagica/spells/components/triggers/TouchTrigger.java
@@ -44,6 +44,7 @@ public class TouchTrigger extends InstantTrigger {
 	@Override
 	protected TriggerData getTargetData(SpellState state, World world,
 				Vec3d pos, float pitch, float yaw) {
+		pos = pos.addVector(0, state.getCaster().getEyeHeight(), 0);
 		
 		RayTraceResult trace = RayTrace.raytrace(world, pos, pitch, yaw, TOUCH_RANGE, true);
 		
@@ -54,12 +55,11 @@ public class TouchTrigger extends InstantTrigger {
 		List<EntityLivingBase> others = Lists.newArrayList(state.getSelf());
 		if (trace.typeOfHit == RayTraceResult.Type.ENTITY && trace.entityHit != null) {
 			// Cast is safe from 'onlyLiving' option in trace
-			System.out.println("entity");
 			return new TriggerData(Lists.newArrayList((EntityLivingBase) trace.entityHit), others, world, null);
 		} else if (trace.typeOfHit == RayTraceResult.Type.BLOCK) {
 			Vec3d vec = trace.hitVec;
 			return new TriggerData(null, others, world,
-					Lists.newArrayList(new BlockPos(Math.round(vec.xCoord), Math.round(vec.yCoord), Math.round(vec.zCoord))));
+					Lists.newArrayList(new BlockPos(Math.floor(vec.xCoord), Math.floor(vec.yCoord), Math.floor(vec.zCoord))));
 		} else {
 			return new TriggerData(null, null, null, null);
 		}
