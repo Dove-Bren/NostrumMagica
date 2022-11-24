0d579e499e7f7a66bcd0f7c7ef977f368c96aa4b
Skyler Manzanares <skymanzanares@hotmail.com>
2022-11-24 10:52:46 -0800

    Broke aggro table into its own class and made the dragon aggro table AI target s election use that. also added 'decaying' when you can't be seen.

---
+++ src/main/java/com/smanzana/nostrummagica/entity/AggroTable.java
@@ -0,0 +1,96 @@
+package com.smanzana.nostrummagica.entity;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.Predicate;
+
+import com.google.common.collect.Lists;
+
+import net.minecraft.entity.EntityLivingBase;
+
+/**
+ * Generic aggro table
+ * @author Skyler
+ *
+ * @param <E>
+ * @param <T>
+ */
+public class AggroTable<T extends EntityLivingBase> {
+
+	private final Map<T, Float> damageTable;
+	
+	// Doesn't prevent entities from being added, but starts reducing contribution when this is false AND
+	// refuses to return them if it returns false
+	private final Predicate<T> filter;
+	
+	public AggroTable(Predicate<T> filter) {
+		damageTable = new HashMap<>();
+		this.filter = filter;
+	}
+
+	public synchronized T getMainTarget() {
+		if (this.damageTable.isEmpty()) {
+			return null;
+		}
+		
+		T target = null;
+		List<Entry<T, Float>> rows = Lists.newArrayList(damageTable.entrySet());
+		Collections.sort(rows, new Comparator<Entry<T, Float>>() {
+			@Override
+			public int compare(Entry<T, Float> arg0, Entry<T, Float> arg1) {
+				if (arg0 == null || arg1 == null) {
+					return arg0 == arg1 ? 0 : -1;
+				}
+				
+				return (int) ((arg1.getValue() * 100.0f) - (arg0.getValue() * 100.0f)); 
+			}
+		});
+		
+		for (Entry<T, Float> row: rows) {
+			T ent = row.getKey();
+			if (this.filter.test(ent)) {
+				target = ent;
+				break;
+			}
+		}
+		
+		return target;
+	}
+	
+	public synchronized void addDamage(T attacker, float damage) {
+		Float val = damageTable.get(attacker);
+		if (val == null) {
+			val = new Float(0.0f);
+		}
+		
+		float real = val.floatValue();
+		real += damage;
+		
+		damageTable.put(attacker, real);
+	}
+	
+	public synchronized void decayTick() {
+		List<T> removeList = new ArrayList<>();
+		
+		for (Entry<T, Float> row: damageTable.entrySet()) {
+			T ent = row.getKey();
+			if (!this.filter.test(ent)) {
+				// Reduce contribution
+				float amt = Math.max(.25f, row.getValue() / (20 * 10f)); // 10 seconds of not seeing you
+				row.setValue(row.getValue() - amt);
+				if (row.getValue() <= 0) {
+					removeList.add(ent);
+				}
+			}
+		}
+		
+		for (T ent : removeList) {
+			damageTable.remove(ent);
+		}
+	}
+}
--- src/main/java/com/smanzana/nostrummagica/entity/tasks/dragon/DragonAIAggroTable.java
+++ src/main/java/com/smanzana/nostrummagica/entity/tasks/dragon/DragonAIAggroTable.java
@@ -1,13 +1,6 @@
 package com.smanzana.nostrummagica.entity.tasks.dragon;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import com.google.common.collect.Lists;
+import com.smanzana.nostrummagica.entity.AggroTable;
 import com.smanzana.nostrummagica.entity.dragon.EntityDragon;
 
 import net.minecraft.entity.EntityLivingBase;
@@ -15,14 +8,16 @@ import net.minecraft.entity.ai.EntityAIBase;
 
 public class DragonAIAggroTable<E extends EntityDragon, T extends EntityLivingBase> extends EntityAIBase {
 
-	private E dragon;
-	private Map<T, Float> damageTable;
-	private boolean checkSight;
+	private final E dragon;
+	private AggroTable<T> aggroTable;
+	private final boolean checkSight;
 	
 	public DragonAIAggroTable(E dragon, boolean checkSight) {
-		damageTable = new HashMap<>();
 		this.checkSight = checkSight;
 		this.dragon = dragon;
+		aggroTable = new AggroTable<>((ent) -> {
+			return !DragonAIAggroTable.this.checkSight || DragonAIAggroTable.this.dragon.getEntitySenses().canSee(ent);
+		});
 	}
 
 	@Override
@@ -30,6 +25,9 @@ public class DragonAIAggroTable<E extends EntityDragon, T extends EntityLivingBa
 		if (dragon == null || dragon.isDead) {
 			return false;
 		}
+		
+		// not a great place for this
+		aggroTable.decayTick();
 		
 		EntityLivingBase current = dragon.getAttackTarget();
 		T targ = this.getTarget();
@@ -43,43 +41,10 @@ public class DragonAIAggroTable<E extends EntityDragon, T extends EntityLivingBa
 	}
 	
 	private T getTarget() {
-		if (this.damageTable.isEmpty()) {
-			return null;
-		}
-		
-		T target = null;
-		List<Entry<T, Float>> rows = Lists.newArrayList(damageTable.entrySet());
-		Collections.sort(rows, new Comparator<Entry<T, Float>>() {
-			@Override
-			public int compare(Entry<T, Float> arg0, Entry<T, Float> arg1) {
-				if (arg0 == null || arg1 == null) {
-					return arg0 == arg1 ? 0 : -1;
-				}
-				
-				return (int) ((arg1.getValue() * 100.0f) - (arg0.getValue() * 100.0f)); 
-			}
-		});
-		
-		for (Entry<T, Float> row: rows) {
-			T ent = row.getKey();
-			if (!this.checkSight || dragon.getEntitySenses().canSee(ent)) {
-				target = ent;
-				break;
-			}
-		}
-		
-		return target;
+		return aggroTable.getMainTarget();
 	}
 	
 	public void addDamage(T attacker, float damage) {
-		Float val = damageTable.get(attacker);
-		if (val == null) {
-			val = new Float(0.0f);
-		}
-		
-		float real = val.floatValue();
-		real += damage;
-		
-		damageTable.put(attacker, real);
+		aggroTable.addDamage(attacker, damage);
 	}
 }
