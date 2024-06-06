6f965a284e26ba10c5d195a95ac34bd28390659c
Skyler Manzanares <skymanzanares@hotmail.com>
2024-06-06 16:07:06 -0700

    Cleanup skill.java warnings

--- src/main/java/com/smanzana/nostrummagica/progression/skill/NostrumSkills.java
+++ src/main/java/com/smanzana/nostrummagica/progression/skill/NostrumSkills.java
@@ -109,71 +109,66 @@ public class NostrumSkills {
 	public static /*final*/ Skill Ender_Master;
 	
 	public static void init() {
-		// Nothing to do but make sure this file is loaded
+		Skill.ClearSkills();
+		int x = -1;
+		int y = 1;
+		Spellcraft_TwoShapes = new Skill(ID_SPELLCRAFT_TWOSHAPES, Category_SpellCrafting, null, () -> SpellRune.getRune(NostrumSpellShapes.Touch), x, y);
+		Spellcraft_ElemBuilding = new Skill(ID_SPELLCRAFT_ELEMBUILDING, Category_SpellCrafting, ID_SPELLCRAFT_TWOSHAPES, () -> SpellRune.getRune(EMagicElement.ICE), x-1, y+1);
+		Spellcraft_ElemWeight = new Skill(ID_SPELLCRAFT_ELEMWEIGHT, Category_SpellCrafting, ID_SPELLCRAFT_ELEMBUILDING, () -> new ItemStack(Items.FEATHER), x-2, y+1);
+		Spellcraft_Weight1 = new Skill(ID_SPELLCRAFT_WEIGHT1, Category_SpellCrafting, ID_SPELLCRAFT_TWOSHAPES, () -> new ItemStack(NostrumItems.masteryOrb), x-1, y-1);
+		Spellcraft_ManaDiscount1 = new Skill(ID_SPELLCRAFT_MANADISCOUNT1, Category_SpellCrafting, ID_SPELLCRAFT_WEIGHT1, () -> new ItemStack(Items.BOOK), x-2, y-1);
+		Spellcraft_ManaDiscount2 = new Skill(ID_SPELLCRAFT_MANADISCOUNT2, Category_SpellCrafting, ID_SPELLCRAFT_MANADISCOUNT1, () -> new ItemStack(Items.WRITABLE_BOOK), x-3, y-1);
 		
-		int unused; // testing code
-		{
-			Skill.ClearSkills();
-			int x = -1;
-			int y = 1;
-			Spellcraft_TwoShapes = new Skill(ID_SPELLCRAFT_TWOSHAPES, Category_SpellCrafting, null, () -> SpellRune.getRune(NostrumSpellShapes.Touch), x, y);
-			Spellcraft_ElemBuilding = new Skill(ID_SPELLCRAFT_ELEMBUILDING, Category_SpellCrafting, ID_SPELLCRAFT_TWOSHAPES, () -> SpellRune.getRune(EMagicElement.ICE), x-1, y+1);
-			Spellcraft_ElemWeight = new Skill(ID_SPELLCRAFT_ELEMWEIGHT, Category_SpellCrafting, ID_SPELLCRAFT_ELEMBUILDING, () -> new ItemStack(Items.FEATHER), x-2, y+1);
-			Spellcraft_Weight1 = new Skill(ID_SPELLCRAFT_WEIGHT1, Category_SpellCrafting, ID_SPELLCRAFT_TWOSHAPES, () -> new ItemStack(NostrumItems.masteryOrb), x-1, y-1);
-			Spellcraft_ManaDiscount1 = new Skill(ID_SPELLCRAFT_MANADISCOUNT1, Category_SpellCrafting, ID_SPELLCRAFT_WEIGHT1, () -> new ItemStack(Items.BOOK), x-2, y-1);
-			Spellcraft_ManaDiscount2 = new Skill(ID_SPELLCRAFT_MANADISCOUNT2, Category_SpellCrafting, ID_SPELLCRAFT_MANADISCOUNT1, () -> new ItemStack(Items.WRITABLE_BOOK), x-3, y-1);
-			
-			x = 1;
-			y = 1;
-			Spellcasting_ElemFree = new Skill(ID_SPELLCAST_ELEMFREE, Category_SpellCasting, null, () -> new ItemStack(NostrumItems.mageStaff), x, y+0);
-			Spellcasting_ElemWeight = new Skill(ID_SPELLCAST_ELEMWEIGHT, Category_SpellCasting, ID_SPELLCAST_ELEMFREE, () -> new ItemStack(NostrumItems.crystalSmall), x+1, y+1);
-			Spellcasting_Weight1 = new Skill(ID_SPELLCAST_WEIGHT1, Category_SpellCasting, ID_SPELLCAST_ELEMWEIGHT, () -> new ItemStack(NostrumItems.reagentSpiderSilk), x+2, y+1);
-			Spellcasting_Potency1 = new Skill(ID_SPELLCAST_POTENCY1, Category_SpellCasting, ID_SPELLCAST_ELEMWEIGHT, () -> new ItemStack(NostrumItems.infusedGemUnattuned), x+2, y);
-			Spellcasting_ElemMana = new Skill(ID_SPELLCAST_ELEMMANA, Category_SpellCasting, ID_SPELLCAST_ELEMFREE, () -> new ItemStack(NostrumItems.spellScroll), x+1, y-1);
-			Spellcasting_ElemLinger = new Skill(ID_SPELLCAST_ELEMLINGER, Category_SpellCasting, ID_SPELLCAST_ELEMMANA, () -> new ItemStack(NostrumItems.thanosStaff), x+2, y-1);
-			
-			x = 0;
-			y = -1;
-			Physical_Novice = new HiddenSkill(ID_PHYSICAL_NOVICE, Category_Element_Physical, null, () -> SpellRune.getRune(EMagicElement.PHYSICAL), x, y, new ElementMasteryRequirement(EMagicElement.PHYSICAL, EElementalMastery.NOVICE));
-			Physical_Adept = new HiddenSkill(ID_PHYSICAL_ADEPT, Category_Element_Physical, ID_PHYSICAL_NOVICE, () -> new ItemStack(Items.IRON_CHESTPLATE), x, y-1, new ElementMasteryRequirement(EMagicElement.PHYSICAL, EElementalMastery.ADEPT));
-			Physical_Master = new HiddenSkill(ID_PHYSICAL_MASTER, Category_Element_Physical, ID_PHYSICAL_ADEPT, () -> new ItemStack(Items.NETHERITE_SWORD), x, y-2, new ElementMasteryRequirement(EMagicElement.PHYSICAL, EElementalMastery.MASTER));
-			
-			x = -1;
-			y = -1;
-			Fire_Novice = new HiddenSkill(ID_FIRE_NOVICE, Category_Element_Fire, null, () -> SpellRune.getRune(EMagicElement.FIRE), x, y, new ElementMasteryRequirement(EMagicElement.FIRE, EElementalMastery.NOVICE));
-			Fire_Adept = new HiddenSkill(ID_FIRE_ADEPT, Category_Element_Fire, ID_FIRE_NOVICE, () -> new ItemStack(NostrumItems.infusedGemFire), x, y-1, new ElementMasteryRequirement(EMagicElement.FIRE, EElementalMastery.ADEPT));
-			Fire_Master = new HiddenSkill(ID_FIRE_MASTER, Category_Element_Fire, ID_FIRE_ADEPT, () -> new ItemStack(Items.SOUL_CAMPFIRE), x, y-2, new ElementMasteryRequirement(EMagicElement.FIRE, EElementalMastery.MASTER));
-			
-			x = 1;
-			y = -1;
-			Ice_Novice = new HiddenSkill(ID_ICE_NOVICE, Category_Element_Ice, null, () -> SpellRune.getRune(EMagicElement.ICE), x, y, new ElementMasteryRequirement(EMagicElement.ICE, EElementalMastery.NOVICE));
-			Ice_Adept = new HiddenSkill(ID_ICE_ADEPT, Category_Element_Ice, ID_ICE_NOVICE, () -> new ItemStack(NostrumItems.infusedGemIce), x, y-1, new ElementMasteryRequirement(EMagicElement.ICE, EElementalMastery.ADEPT));
-			Ice_Master = new HiddenSkill(ID_ICE_MASTER, Category_Element_Ice, ID_ICE_ADEPT, () -> SpellRune.getRune(NostrumSpellShapes.OnHealth), x, y-2, new ElementMasteryRequirement(EMagicElement.ICE, EElementalMastery.MASTER));
-			
-			x = 2;
-			y = -1;
-			Earth_Novice = new HiddenSkill(ID_EARTH_NOVICE, Category_Element_Earth, null, () -> SpellRune.getRune(EMagicElement.EARTH), x, y, new ElementMasteryRequirement(EMagicElement.EARTH, EElementalMastery.NOVICE));
-			Earth_Adept = new HiddenSkill(ID_EARTH_ADEPT, Category_Element_Earth, ID_EARTH_NOVICE, () -> new ItemStack(Items.NETHERITE_AXE), x, y-1, new ElementMasteryRequirement(EMagicElement.EARTH, EElementalMastery.ADEPT));
-			Earth_Master = new HiddenSkill(ID_EARTH_MASTER, Category_Element_Earth, ID_EARTH_ADEPT, () -> new ItemStack(Items.GLISTERING_MELON_SLICE), x, y-2, new ElementMasteryRequirement(EMagicElement.EARTH, EElementalMastery.MASTER));
-			
-			x = -2;
-			y = -1;
-			Wind_Novice = new HiddenSkill(ID_WIND_NOVICE, Category_Element_Wind, null, () -> SpellRune.getRune(EMagicElement.WIND), x, y, new ElementMasteryRequirement(EMagicElement.WIND, EElementalMastery.NOVICE));
-			Wind_Adept = new HiddenSkill(ID_WIND_ADEPT, Category_Element_Wind, ID_WIND_NOVICE, () -> SpellRune.getRune(NostrumSpellShapes.Cutter), x, y-1, new ElementMasteryRequirement(EMagicElement.WIND, EElementalMastery.ADEPT));
-			Wind_Master = new HiddenSkill(ID_WIND_MASTER, Category_Element_Wind, ID_WIND_ADEPT, () -> new ItemStack(NostrumItems.magicCharmWind), x, y-2, new ElementMasteryRequirement(EMagicElement.WIND, EElementalMastery.MASTER));
-			
-			x = -3;
-			y = -1;
-			Lightning_Novice = new HiddenSkill(ID_LIGHTNING_NOVICE, Category_Element_Lightning, null, () -> SpellRune.getRune(EMagicElement.LIGHTNING), x, y, new ElementMasteryRequirement(EMagicElement.LIGHTNING, EElementalMastery.NOVICE));
-			Lightning_Adept = new HiddenSkill(ID_LIGHTNING_ADEPT, Category_Element_Lightning, ID_LIGHTNING_NOVICE, () -> new ItemStack(NostrumItems.enchantedWeaponLightningAdept), x, y-1, new ElementMasteryRequirement(EMagicElement.LIGHTNING, EElementalMastery.ADEPT));
-			Lightning_Master = new HiddenSkill(ID_LIGHTNING_MASTER, Category_Element_Lightning, ID_LIGHTNING_ADEPT, () -> new ItemStack(NostrumItems.magicCharmLightning), x, y-2, new ElementMasteryRequirement(EMagicElement.LIGHTNING, EElementalMastery.MASTER));
-			
-			x = 3;
-			y = -1;
-			Ender_Novice = new HiddenSkill(ID_ENDER_NOVICE, Category_Element_Ender, null, () -> SpellRune.getRune(EMagicElement.ENDER), x, y, new ElementMasteryRequirement(EMagicElement.ENDER, EElementalMastery.NOVICE));
-			Ender_Adept = new HiddenSkill(ID_ENDER_ADEPT, Category_Element_Ender, ID_ENDER_NOVICE, () -> new ItemStack(Items.ENDER_PEARL), x, y-1, new ElementMasteryRequirement(EMagicElement.ENDER, EElementalMastery.ADEPT));
-			Ender_Master = new HiddenSkill(ID_ENDER_MASTER, Category_Element_Ender, ID_ENDER_ADEPT, () -> new ItemStack(Items.ENDER_EYE), x, y-2, new ElementMasteryRequirement(EMagicElement.ENDER, EElementalMastery.MASTER));
-		}
+		x = 1;
+		y = 1;
+		Spellcasting_ElemFree = new Skill(ID_SPELLCAST_ELEMFREE, Category_SpellCasting, null, () -> new ItemStack(NostrumItems.mageStaff), x, y+0);
+		Spellcasting_ElemWeight = new Skill(ID_SPELLCAST_ELEMWEIGHT, Category_SpellCasting, ID_SPELLCAST_ELEMFREE, () -> new ItemStack(NostrumItems.crystalSmall), x+1, y+1);
+		Spellcasting_Weight1 = new Skill(ID_SPELLCAST_WEIGHT1, Category_SpellCasting, ID_SPELLCAST_ELEMWEIGHT, () -> new ItemStack(NostrumItems.reagentSpiderSilk), x+2, y+1);
+		Spellcasting_Potency1 = new Skill(ID_SPELLCAST_POTENCY1, Category_SpellCasting, ID_SPELLCAST_ELEMWEIGHT, () -> new ItemStack(NostrumItems.infusedGemUnattuned), x+2, y);
+		Spellcasting_ElemMana = new Skill(ID_SPELLCAST_ELEMMANA, Category_SpellCasting, ID_SPELLCAST_ELEMFREE, () -> new ItemStack(NostrumItems.spellScroll), x+1, y-1);
+		Spellcasting_ElemLinger = new Skill(ID_SPELLCAST_ELEMLINGER, Category_SpellCasting, ID_SPELLCAST_ELEMMANA, () -> new ItemStack(NostrumItems.thanosStaff), x+2, y-1);
+		
+		x = 0;
+		y = -1;
+		Physical_Novice = new HiddenSkill(ID_PHYSICAL_NOVICE, Category_Element_Physical, null, () -> SpellRune.getRune(EMagicElement.PHYSICAL), x, y, new ElementMasteryRequirement(EMagicElement.PHYSICAL, EElementalMastery.NOVICE));
+		Physical_Adept = new HiddenSkill(ID_PHYSICAL_ADEPT, Category_Element_Physical, ID_PHYSICAL_NOVICE, () -> new ItemStack(Items.IRON_CHESTPLATE), x, y-1, new ElementMasteryRequirement(EMagicElement.PHYSICAL, EElementalMastery.ADEPT));
+		Physical_Master = new HiddenSkill(ID_PHYSICAL_MASTER, Category_Element_Physical, ID_PHYSICAL_ADEPT, () -> new ItemStack(Items.NETHERITE_SWORD), x, y-2, new ElementMasteryRequirement(EMagicElement.PHYSICAL, EElementalMastery.MASTER));
+		
+		x = -1;
+		y = -1;
+		Fire_Novice = new HiddenSkill(ID_FIRE_NOVICE, Category_Element_Fire, null, () -> SpellRune.getRune(EMagicElement.FIRE), x, y, new ElementMasteryRequirement(EMagicElement.FIRE, EElementalMastery.NOVICE));
+		Fire_Adept = new HiddenSkill(ID_FIRE_ADEPT, Category_Element_Fire, ID_FIRE_NOVICE, () -> new ItemStack(NostrumItems.infusedGemFire), x, y-1, new ElementMasteryRequirement(EMagicElement.FIRE, EElementalMastery.ADEPT));
+		Fire_Master = new HiddenSkill(ID_FIRE_MASTER, Category_Element_Fire, ID_FIRE_ADEPT, () -> new ItemStack(Items.SOUL_CAMPFIRE), x, y-2, new ElementMasteryRequirement(EMagicElement.FIRE, EElementalMastery.MASTER));
+		
+		x = 1;
+		y = -1;
+		Ice_Novice = new HiddenSkill(ID_ICE_NOVICE, Category_Element_Ice, null, () -> SpellRune.getRune(EMagicElement.ICE), x, y, new ElementMasteryRequirement(EMagicElement.ICE, EElementalMastery.NOVICE));
+		Ice_Adept = new HiddenSkill(ID_ICE_ADEPT, Category_Element_Ice, ID_ICE_NOVICE, () -> new ItemStack(NostrumItems.infusedGemIce), x, y-1, new ElementMasteryRequirement(EMagicElement.ICE, EElementalMastery.ADEPT));
+		Ice_Master = new HiddenSkill(ID_ICE_MASTER, Category_Element_Ice, ID_ICE_ADEPT, () -> SpellRune.getRune(NostrumSpellShapes.OnHealth), x, y-2, new ElementMasteryRequirement(EMagicElement.ICE, EElementalMastery.MASTER));
+		
+		x = 2;
+		y = -1;
+		Earth_Novice = new HiddenSkill(ID_EARTH_NOVICE, Category_Element_Earth, null, () -> SpellRune.getRune(EMagicElement.EARTH), x, y, new ElementMasteryRequirement(EMagicElement.EARTH, EElementalMastery.NOVICE));
+		Earth_Adept = new HiddenSkill(ID_EARTH_ADEPT, Category_Element_Earth, ID_EARTH_NOVICE, () -> new ItemStack(Items.NETHERITE_AXE), x, y-1, new ElementMasteryRequirement(EMagicElement.EARTH, EElementalMastery.ADEPT));
+		Earth_Master = new HiddenSkill(ID_EARTH_MASTER, Category_Element_Earth, ID_EARTH_ADEPT, () -> new ItemStack(Items.GLISTERING_MELON_SLICE), x, y-2, new ElementMasteryRequirement(EMagicElement.EARTH, EElementalMastery.MASTER));
+		
+		x = -2;
+		y = -1;
+		Wind_Novice = new HiddenSkill(ID_WIND_NOVICE, Category_Element_Wind, null, () -> SpellRune.getRune(EMagicElement.WIND), x, y, new ElementMasteryRequirement(EMagicElement.WIND, EElementalMastery.NOVICE));
+		Wind_Adept = new HiddenSkill(ID_WIND_ADEPT, Category_Element_Wind, ID_WIND_NOVICE, () -> SpellRune.getRune(NostrumSpellShapes.Cutter), x, y-1, new ElementMasteryRequirement(EMagicElement.WIND, EElementalMastery.ADEPT));
+		Wind_Master = new HiddenSkill(ID_WIND_MASTER, Category_Element_Wind, ID_WIND_ADEPT, () -> new ItemStack(NostrumItems.magicCharmWind), x, y-2, new ElementMasteryRequirement(EMagicElement.WIND, EElementalMastery.MASTER));
+		
+		x = -3;
+		y = -1;
+		Lightning_Novice = new HiddenSkill(ID_LIGHTNING_NOVICE, Category_Element_Lightning, null, () -> SpellRune.getRune(EMagicElement.LIGHTNING), x, y, new ElementMasteryRequirement(EMagicElement.LIGHTNING, EElementalMastery.NOVICE));
+		Lightning_Adept = new HiddenSkill(ID_LIGHTNING_ADEPT, Category_Element_Lightning, ID_LIGHTNING_NOVICE, () -> new ItemStack(NostrumItems.enchantedWeaponLightningAdept), x, y-1, new ElementMasteryRequirement(EMagicElement.LIGHTNING, EElementalMastery.ADEPT));
+		Lightning_Master = new HiddenSkill(ID_LIGHTNING_MASTER, Category_Element_Lightning, ID_LIGHTNING_ADEPT, () -> new ItemStack(NostrumItems.magicCharmLightning), x, y-2, new ElementMasteryRequirement(EMagicElement.LIGHTNING, EElementalMastery.MASTER));
+		
+		x = 3;
+		y = -1;
+		Ender_Novice = new HiddenSkill(ID_ENDER_NOVICE, Category_Element_Ender, null, () -> SpellRune.getRune(EMagicElement.ENDER), x, y, new ElementMasteryRequirement(EMagicElement.ENDER, EElementalMastery.NOVICE));
+		Ender_Adept = new HiddenSkill(ID_ENDER_ADEPT, Category_Element_Ender, ID_ENDER_NOVICE, () -> new ItemStack(Items.ENDER_PEARL), x, y-1, new ElementMasteryRequirement(EMagicElement.ENDER, EElementalMastery.ADEPT));
+		Ender_Master = new HiddenSkill(ID_ENDER_MASTER, Category_Element_Ender, ID_ENDER_ADEPT, () -> new ItemStack(Items.ENDER_EYE), x, y-2, new ElementMasteryRequirement(EMagicElement.ENDER, EElementalMastery.MASTER));
 	}
 	
 }
