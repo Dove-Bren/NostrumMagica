b84c948d10675beff9e4dad72a6ae873d8ca9d05
Skyler Manzanares <skymanzanares@hotmail.com>
2024-06-19 16:28:20 -0700

    Remove some legacy page classes that are unused and causing warnings.

--- src/main/java/com/smanzana/nostrummagica/client/gui/book/TitlePage.java
+++ src/main/java/com/smanzana/nostrummagica/client/gui/book/TitlePage.java
@@ -12,38 +12,27 @@ import net.minecraft.client.gui.FontRenderer;
  */
 public class TitlePage implements IBookPage {
 	
-	private ImagePage image;
 	private String text;
 	private boolean index;
 	private int hoffset = -1;
 	
 	public TitlePage(String text, boolean index) {
-		this(text, null, index);
-	}
-	
-	public TitlePage(String text, ImagePage image, boolean index) {
 		this.text = text;
 		this.index = index;
-		this.image = image;
 	}
 
 	@Override
 	public void draw(BookScreen parent, MatrixStack matrixStackIn, FontRenderer fonter, int xoffset, int yoffset, int width, int height) {
-		if (image == null) {
 			if (hoffset == -1) {
 				int w = fonter.getStringWidth(this.text);
 				hoffset = w/2; 
 			}
 			fonter.drawString(matrixStackIn, text, xoffset + ( (width / 2) - hoffset ), yoffset + 20, 0xFF106020);
-		} else {
-			image.draw(parent, matrixStackIn, fonter, xoffset, yoffset, width, height);
-		}
 	}
 
 	@Override
 	public void overlay(BookScreen parent, MatrixStack matrixStackIn, FontRenderer fonter, int mouseX, int mouseY, int trueX, int trueY) {
-		if (image != null)
-			image.overlay(parent, matrixStackIn, fonter, mouseX, mouseY, trueX, trueY);
+		;
 	}
 	
 	public String getTitle() {
