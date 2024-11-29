a156ece1ac3c4023cd4bf124c42f81287a497da5
Skyler Manzanares <skymanzanares@hotmail.com>
2024-11-29 11:45:14 -0800

    Redo chalk block models/textures to be less jarring

--- src/main/java/com/smanzana/nostrummagica/block/ChalkBlock.java
+++ src/main/java/com/smanzana/nostrummagica/block/ChalkBlock.java
@@ -3,7 +3,12 @@ package com.smanzana.nostrummagica.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.FourWayBlock;
 import net.minecraft.block.material.Material;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.StateContainer;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
@@ -11,23 +16,37 @@ import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.IWorldReader;
+import net.minecraft.world.World;
 
-public class ChalkBlock extends Block {
+public class ChalkBlock extends FourWayBlock {
 
 	public static final String ID = "nostrum_chalk_block";
 	protected static final VoxelShape CHALK_AABB = Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 16 * 0.03125D, 16.0D);
 	
 	public ChalkBlock() {
-		super(Block.Properties.create(Material.CARPET)
+		super(0, 0, 16, 16, 16, Block.Properties.create(Material.CARPET)
 				.hardnessAndResistance(.01f)
 				.setLightLevel((state) -> 1)
 				.noDrops()
 				.notSolid()
 				);
+		
+		this.setDefaultState(this.stateContainer.getBaseState().with(NORTH, false).with(EAST, false).with(SOUTH, false).with(WEST, false).with(WATERLOGGED, false));;
+	}
+	
+	@Override
+	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+		super.fillStateContainer(builder);
+		builder.add(NORTH, EAST, WEST, SOUTH, WATERLOGGED);
 	}
 	
 	@Override
 	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+		return CHALK_AABB;
+	}
+	
+	@Override
+	public VoxelShape getCollisionShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
 		return CHALK_AABB;
 	}
 	
@@ -39,7 +58,40 @@ public class ChalkBlock extends Block {
 	@SuppressWarnings("deprecation")
 	@Override
 	public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
-		return facing == Direction.DOWN && !this.isValidPosition(stateIn, worldIn, currentPos) ? Blocks.AIR.getDefaultState() : super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
+		if (stateIn.get(WATERLOGGED)) {
+			worldIn.getPendingFluidTicks().scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickRate(worldIn));
+		}
+		
+		// If below changed, possibly break
+		if (facing == Direction.DOWN) {
+			if (!this.isValidPosition(stateIn, worldIn, currentPos)) {
+				return Blocks.AIR.getDefaultState();
+			}
+		}
+		
+		if (facing.getAxis().getPlane() == Direction.Plane.HORIZONTAL) {
+			return stateIn.with(FACING_TO_PROPERTY_MAP.get(facing), Boolean.valueOf(this.canConnect(facingState)));
+		}
+		
+		return super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
+	}
+	
+	protected boolean canConnect(BlockState state) {
+		return state.getBlock() == this;
+	}
+	
+	@Override
+	public BlockState getStateForPlacement(BlockItemUseContext context) {
+		final World world = context.getWorld();
+		final BlockPos pos = context.getPos();
+		final FluidState fluidstate = world.getFluidState(pos);
+		return super.getStateForPlacement(context)
+				.with(NORTH, canConnect(world.getBlockState(pos.north())))
+				.with(EAST, canConnect(world.getBlockState(pos.east())))
+				.with(SOUTH, canConnect(world.getBlockState(pos.south())))
+				.with(WEST, canConnect(world.getBlockState(pos.west())))
+				.with(WATERLOGGED, Boolean.valueOf(fluidstate.getFluid() == Fluids.WATER))
+				;
 	}
 	
 //	@Override
